d3036f5439e3593cdb2fb8f728348dc8ad4902ed
Данис Шакиров <shakirovdanis05@gmail.com>
2026-04-26 14:00:57 +0300

    used ids instead of entities

--- FinanceBot/FinBot.Bll.Interfaces/Services/IUserService.cs
+++ FinanceBot/FinBot.Bll.Interfaces/Services/IUserService.cs
@@ -6,16 +6,14 @@ namespace FinBot.Bll.Interfaces.Services;
 
 public interface IUserService
 {
-    // Task<Result<User?>> GetUserByTgIdAsync(long id);
-    // Task<Result<User?>> GetUserByGuidIdAsync(Guid id);
     Task<Result<User>> CreateUserAsync(long tgId, string displayName);
     Task<Result<User>> GetOrCreateUserAsync(long tgId, string displayName);
     Task<Result<decimal>> AddExpenseAsync(
-        User user,
+        Guid userId,
         Guid groupId,
         decimal amount,
         ExpenseCategory category);
 
-    Task<Result<User?>> GetUserByGuidIdAsync(Guid id);
-    Task<Result<User?>> GetUserByTgIdAsync(long id);
+    Task<Result<User>> GetUserByGuidIdAsync(Guid userId);
+    Task<Result<User>> GetUserByTgIdAsync(long userId);
 }
